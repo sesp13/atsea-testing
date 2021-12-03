45faee41216e6b5c0eb9443a719678dd48b9bebe
sesp13 <sespinosarte99@gmail.com>
2021-12-02 22:25:21 -0500

    add get customer by id and username

--- test/api/customer-requests.test.ts
+++ test/api/customer-requests.test.ts
@@ -10,13 +10,13 @@ const baseUrl = `http://localhost:8080/api/customer/`;
 
 // Global variables
 const customerBody = {
-  customerId: 5000,
-  name: 'Sally Vallery',
+  customer_id: undefined,
+  name: 'Juan Carlos',
   address: '144 Townsend, San Francisco 99999',
-  email: 'sally@example.com',
+  email: 'juanca548@example.com',
   phone: '513 222 5555',
-  username: 'sallyv',
-  password: 'sallypassword',
+  username: 'juanca548',
+  password: 'juancapassword',
   enabled: 'true',
   role: 'USER',
 };
@@ -24,15 +24,53 @@ const customerBody = {
 // Test body
 
 describe('Customer endpoints tests', () => {
-  let customerFetched;
-
-  it('Get user by id', async () => {
+  it('Create Customer', async () => {
     try {
-      const response = await get(`${baseUrl}${customerBody.customerId}`);
+      const response = await post(baseUrl).send(customerBody);
+      expect(response.status).to.equal(StatusCodes.CREATED);
+      expect(response.body).to.be.an('object');
+      expect(response.body).to.haveOwnProperty('customerId');
+      customerBody.customer_id = response.body.customerId;
+    } catch (error) {
+      // Behavior if the user already exists
+      expect(error.status).to.equal(StatusCodes.CONFLICT);
+      const body = ApiHelpers.expectErrorStructure(error);
+      expect(body.errorMessage).to.equal(
+        `A customer with username ${customerBody.username} already exists.`
+      );
+    }
+  });
+
+  it('Get customer by username', async () => {
+    try {
+      const response = await get(`${baseUrl}username=${customerBody.username}`);
       expect(response.status).to.equal(StatusCodes.OK);
-      customerFetched = response.body;
+      const customerFetched = response.body;
       expect(customerFetched).to.be.an('object');
-      expect(customerFetched).to.haveOwnProperty('customerId');
+      expect(customerFetched).to.haveOwnProperty('customerIf');
+      expect(customerFetched).to.haveOwnProperty('name');
+      expect(customerFetched).to.haveOwnProperty('address');
+      expect(customerFetched).to.haveOwnProperty('email');
+      expect(customerFetched).to.haveOwnProperty('username');
+      expect(customerFetched).to.haveOwnProperty('phone');
+      customerBody.customer_id = customerFetched.customerIf;
+    } catch (error) {
+      // Behavior if the customer doesn't exists
+      expect(error.status).to.equal(StatusCodes.NOT_FOUND);
+      const body = ApiHelpers.expectErrorStructure(error);
+      expect(body.errorMessage).to.equal(
+        `Customer with usernama ${customerBody.username} not found`
+      );
+    }
+  });
+
+  it('Get customer by id', async () => {
+    try {
+      const response = await get(`${baseUrl}${customerBody.customer_id}`);
+      expect(response.status).to.equal(StatusCodes.OK);
+      const customerFetched = response.body;
+      expect(customerFetched).to.be.an('object');
+      expect(customerFetched).to.haveOwnProperty('customerIf');
       expect(customerFetched).to.haveOwnProperty('name');
       expect(customerFetched).to.haveOwnProperty('address');
       expect(customerFetched).to.haveOwnProperty('email');
@@ -43,25 +81,7 @@ describe('Customer endpoints tests', () => {
       expect(error.status).to.equal(StatusCodes.NOT_FOUND);
       const body = ApiHelpers.expectErrorStructure(error);
       expect(body.errorMessage).to.equal(
-        `Customer with id ${customerBody.customerId} not found`
-      );
-    }
-  });
-
-  it('Create Customer', async () => {
-    try {
-      const response = await post(baseUrl).send(customerBody);
-      expect(response.status).to.equal(StatusCodes.CREATED);
-      expect(response.body).to.be.an('object');
-      expect(response.body).to.haveOwnProperty('customerId');
-      expect(response.body.customerId).to.equal(customerBody.customerId);
-    } catch (error) {
-      // Behavior if the user already exists
-      expect(error.status).to.equal(StatusCodes.CONFLICT);
-      const body = ApiHelpers.expectErrorStructure(error);
-      console.log(body);
-      expect(body.errorMessage).to.equal(
-        `A customer with username ${customerBody.username} already exists.`
+        `Customer with id ${customerBody.customer_id} not found`
       );
     }
   });
