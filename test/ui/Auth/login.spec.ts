840f511b32cc440be24b24a6601b3c4a952e6498
sesp13 <sespinosarte99@gmail.com>
2021-12-09 23:02:09 -0500

    correct problems with imports

--- test/ui/Auth/login.spec.ts
+++ test/ui/Auth/login.spec.ts
@@ -1,14 +1,12 @@
 //import { expect } from 'chai';
 import { browser } from 'protractor';
-import { CustomerHelpers } from 'test/api/helpers/CustomerHelpers';
-import { customerSample } from 'test/api/helpers/GlobalInformation';
+import { CustomerHelpers } from '../../../test/api/helpers/CustomerHelpers';
+import { customerSample } from '../../../test/api/helpers/GlobalInformation';
 import { HeaderPage } from '../helpers/headerPage';
-
-
 
 let customerBody = customerSample;
 describe('Sign in proccess', () => {
-  const headerPage: HeaderPage = new HeaderPage()
+  const headerPage: HeaderPage = new HeaderPage();
   beforeEach(async () => {
     const userUrl = `http://localhost:8080/api/customer/`;
     await browser.get('http://host.docker.internal:8080');
@@ -17,8 +15,9 @@ describe('Sign in proccess', () => {
       customerBody
     );
   });
-  it('open sign in modal'), async () => {
-    headerPage.openSignInModal()
-    await browser.sleep(3000)
-  }
-})
+  it('open sign in modal'),
+    async () => {
+      headerPage.openSignInModal();
+      await browser.sleep(3000);
+    };
+});
