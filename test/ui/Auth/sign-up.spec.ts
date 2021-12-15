8a79b060ec9e1a6f38e1893b1cdf696a21fc8ed6
sesp13 <sespinosarte99@gmail.com>
2021-12-14 22:53:01 -0500

    add more time to the hook

--- test/ui/Auth/sign-up.spec.ts
+++ test/ui/Auth/sign-up.spec.ts
@@ -17,14 +17,14 @@ describe('Sign up process', () => {
   beforeEach(async () => {
     await browser.get(GlobalInformation.dockerInternalUrl);
     await headerPage.openSignUpModal();
-    await browser.sleep(5000);
+    await browser.sleep(15000);
   });
 
   it('Signup process', async () => {
     await signUpPage.fillformAndSignUp(userBody.username, userBody.password);
     await browser.wait(
       ExpectedConditions.visibilityOf(await signUpPage.getSuccessBtn()),
-      5000
+      15000
     );
     expect(await signUpPage.getSuccessMessage().getText()).to.equal(
       'Congratulations! Your account has been created!'
